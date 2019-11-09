16564ee13725dc1a2d9fb8fbc85d7a769eb4d6f1
Daniel-Auger-Pro <dauger@gmail.com>
2019-11-09 09:06:51 -0500

    Upload-20191109

--- Programmation1/Chapitre01/20191024.LectureEcriture/js/exercice15.js
+++ Programmation1/Chapitre01/20191024.LectureEcriture/js/exercice15.js
@@ -5,6 +5,8 @@
 // Étape 1 : Trouver les variables.
 
 // "Ce que j'avais fait! :" var a; var b; var c; var x; var y;
+// Version de Shany
+
 var a,b,c,x,y;
 
 //  Étape 2 : Lecture (demander à l'usager des données)
@@ -17,9 +19,13 @@ x = Number(prompt("Entrez la valeur de X : "));
 // Étape 3 : Le calcule
 
 // "Ce que j'avais fait! :" y = (a * (x * x)) + (b * x) + c;
-y = (a * )
+// La version de Shany
+
+y = a * Math.pow(x,2) + b*x + c;
 
 // Étape 4 : AFFICHE les résultats
+// Le document.write vient de Shany
 
 console.log("La valeur de Y est : " + y +".");
+// Ne Fonctionne pas "document.write("Voici la valeur de Y : " + y);"
 alert("La valeur de Y est : " + y +".");
